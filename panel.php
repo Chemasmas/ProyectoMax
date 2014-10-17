e383a681181a7a54254c6b3b77e880e398327e64
luis1159 <luis11_59@hotmail.com>
2014-10-17 10:48:06 -0500

    estilos
    agregue estilos

--- panel.php
+++ panel.php
@@ -3,6 +3,7 @@
 </head>
 <link rel="stylesheet" href="css/editor.css">
 <script src="js/pruebas.js"></script>
+<script src="js/modificadores.js"></script>
 <body>
 	<div id="barraH">
 		<div id="opc1" class="botonB"></div>
@@ -14,7 +15,53 @@
 		<div id="opc7" class="botonB"></div>
 		<div id="opc8" class="botonB"></div>
 	</div>
-	<div id="vistaPrevia"></div>
-	<div id="modificadores"></div>
+	<div id="vistaPrevia">
+    <div class="tabs">
+    <input checked id="one" name="tabs" type="radio">
+    <label for="one">Panel 1</label>
+    <input id="two" name="tabs" type="radio" value="Two">
+    <label for="two">Panel 2</label>
+    <input id="three" name="tabs" type="radio">
+    <label for="three">Panel 3</label>
+    <input id="four" name="tabs" type="radio">
+    <label for="four">Panel 4</label>
+    <div class="panels">
+        <div class="panel">
+            	<h2>Panel 1</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel leo sem. Sed nec quam sit amet lorem volutpat ullamcorper ut sed massa. Sed vel felis velit. Nullam et turpis sed dui auctor vehicula quis a dui.</p>
+            <p>Aliquam vitae leo et sapien volutpat accumsan quis eget turpis. Etiam ac metus vitae purus semper pretium. Curabitur id nisl nisl. Cras ut massa sed dolor ullamcorper consequat ut sed dolor.</p>
+            
+        </div>
+        <div class="panel">
+            	<h2>Panel 2</h2>
+
+            <p>Aliquam vitae leo et sapien volutpat accumsan quis eget turpis. Etiam ac metus vitae purus semper pretium. Curabitur id nisl nisl. Cras ut massa sed dolor ullamcorper consequat ut sed dolor.</p>
+            <p>Nam sodales, mi eu convallis adipiscing, ligula justo consectetur tellus, tincidunt vestibulum tortor elit a augue. Ut elementum ultricies orci, vel luctus neque varius in. Phasellus turpis nunc, eleifend ac fringilla at, malesuada in eros.</p>
+        </div>
+        <div class="panel">
+            	<h2>Panel 3</h2>
+
+            <p>Nam sodales, mi eu convallis adipiscing, ligula justo consectetur tellus, tincidunt vestibulum tortor elit a augue. Ut elementum ultricies orci, vel luctus neque varius in. Phasellus turpis nunc, eleifend ac fringilla at, malesuada in eros.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel leo sem. Sed nec quam sit amet lorem volutpat ullamcorper ut sed massa. Sed vel felis velit. Nullam et turpis sed dui auctor vehicula quis a dui.</p>
+            
+        </div>
+        <div class="panel">
+            	<h2>Panel 4</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel leo sem. Sed nec quam sit amet lorem volutpat ullamcorper ut sed massa. Sed vel felis velit. Nullam et turpis sed dui auctor vehicula quis a dui.</p>
+            <p>Aliquam vitae leo et sapien volutpat accumsan quis eget turpis. Etiam ac metus vitae purus semper pretium. Curabitur id nisl nisl. Cras ut massa sed dolor ullamcorper consequat ut sed dolor.</p>
+        </div>
+    </div>
+</div>
+</div>
+	<div id="modificadores">
+<fieldset>
+  <legend>Colores</legend>
+  <div class="colorpit"></div>
+</fieldset>
+       </div>
+</section>
+    </div>
 </body>
 </html>
