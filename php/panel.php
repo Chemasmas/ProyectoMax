b5be4ce20cda2bf28813eb6bea5f54ab5e9746b0
OswaldoCuzSimon <oswaldo_cs_94@hotmail.com>
2014-12-10 21:59:06 -0600

    iconos

--- php/panel.php
+++ php/panel.php
@@ -4,15 +4,16 @@
 <div id="vistaPrevia">
 </div>
 <div id="tools">
-    <ul>
-        <li><a href="#texto">T</a></li>
-        <li><a href="#tabs-2">I</a></li>
+    <ul id="toolsL">
+        <li id="estiloE"><a href="#texto"></a></li>
+        <li id="imagenE"><a href="#editar_imagen"></a></li>
     </ul>
     <div id="scroll">
         <div id="texto">
             <?php include "customize.php" ?>
         </div>
-    <div id="tabs-2">
-            <?php include "imagen.php" ?>
-        
+        <div id="editar_imagen">
+                <?php include "imagen.php" ?>
+        </div>
     </div>
+</div>
